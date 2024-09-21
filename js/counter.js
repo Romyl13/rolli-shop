210f97fe6def301ed7a0feef043e3a5d1c11e215
Roman <svinka34555@gmail.com>
2024-09-21 19:13:00 +0300

    add counter.js and a script was written to increase or decrease the item

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,35 @@
-console.log('Done');
+window.addEventListener('click', (event) => {/* відділяємо кліки крч, зараз кілки є по всій сторінці але в середину коду пропишем щоб щотчик редагувався тільки при кліку на кнопки - чи + */
+    /* console.log(event.target); - тут описується на якому тегові було нажато (img div і тд)*/
+    if (event.target.dataset.action  === 'plus' /* dataset.назва атрибуту - таким способом можна обратитись до назви атрибуту */) {
+        //зараз ми короче повертаємось до батька дата-атрибуту, потім в батьковому блоці шукаємо атриюут data-counter в якому записано число
+        const counterWrapper = event.target.closest('.counter-wrapper');//шукає найближчого батька(closest())
+
+        const counter = counterWrapper.querySelector('[data-counter]');
+        counter.innerText = ++counter.innerText;
+    };
+
+    if (event.target.dataset.action  === 'minus') {
+        const counterWrapper = event.target.closest('.counter-wrapper');
+        const counter = counterWrapper.querySelector('[data-counter]');
+        if (parseInt(counter.innerText) > 1) {
+            counter.innerText = --counter.innerText;
+        };
+    };
+});
+
+
+
+
+/* const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+const counter = document.querySelector('[data-counter]');
+
+btnMinus.addEventListener('click', () => {
+    if (parseInt(counter.innerText) > 1) {
+        counter.innerText = --counter.innerText;
+    };
+});
+
+btnPlus.addEventListener('click', () => {
+    counter.innerText = ++counter.innerText; - робилось все для одної кнопки мінуса і плюса
+}); */
